5a8f7e239000eab6205aaced4567e60b5d965017
Ezequiel Rodrigues Trindade <ezefuts@gmail.com>
2021-06-20 22:57:13 -0300

    Adicionando imagens nas pizzas

--- script.js
+++ script.js
@@ -6,7 +6,8 @@ pizzaJson.map((item, index) => {
 
     pizzaItem.querySelector('.pizza-item--name').innerHTML = item.name;
     pizzaItem.querySelector('.pizza-item--desc').innerHTML = item.description;
-    pizzaItem.querySelector('.pizza-item--price').innerHTML = `R$ ${item.price.toFixed(2)}`;
+    pizzaItem.querySelector('.pizza-item--price').innerHTML = `R$ ${item.price.toFixed(2)}`;    
+    pizzaItem.querySelector('.pizza-item--img img').src = item.img;
 
     c('.pizza-area').append(pizzaItem);
 })
