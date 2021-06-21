bd44981686bcd685a8716ae1c354ed56825ae434
Ezequiel Rodrigues Trindade <ezefuts@gmail.com>
2021-06-20 23:30:34 -0300

    Selecionando item clicado

--- script.js
+++ script.js
@@ -4,13 +4,16 @@ const cs = (el)=>document.querySelectorAll(el);
 pizzaJson.map((item, index) => {
     let pizzaItem = c('.models .pizza-item').cloneNode(true);
 
+    pizzaItem.setAttribute('data-key', index);
     pizzaItem.querySelector('.pizza-item--name').innerHTML = item.name;
     pizzaItem.querySelector('.pizza-item--desc').innerHTML = item.description;
     pizzaItem.querySelector('.pizza-item--price').innerHTML = `R$ ${item.price.toFixed(2)}`;    
     pizzaItem.querySelector('.pizza-item--img img').src = item.img;
     pizzaItem.querySelector('a').addEventListener('click', (e)=>{
         e.preventDefault();
+        let key = e.target.closest('.pizza-item').getAttribute('data-key');
         
+
         c('.pizzaWindowArea').style.opacity = 0;
         c('.pizzaWindowArea').style.display = "flex";
         setTimeout(()=>{
@@ -20,3 +23,4 @@ pizzaJson.map((item, index) => {
 
     c('.pizza-area').append(pizzaItem);
 });
+
