c633572a6bef8f42c4015908a993ac30d51cd3df
Ezequiel Rodrigues Trindade <ezefuts@gmail.com>
2021-06-20 22:50:56 -0300

    Adicionando nome nas pizzas

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const cs = (el)=>document.querySelectorAll(el);
 pizzaJson.map((item, index) => {
     let pizzaItem = c('.models .pizza-item').cloneNode(true);
 
+    pizzaItem.querySelector('.pizza-item--name').innerHTML = item.name;
 
     c('.pizza-area').append(pizzaItem);
 })
